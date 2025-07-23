926c194e210a2ab5d43d0e602c0006406ee490b8
aryan-jr-07 <aryanjr2010@gmail.com>
2025-07-24 00:01:34 +0530

    variables

--- module 1/start.js
+++ module 1/start.js
@@ -1,6 +1,12 @@
 // variables -- variable are container for storing data (storing data values)
-// variables are of three types -- var, let, const
+// rules for naming variables:
+// 1. variable name can only contain letter, numbers, underscores, and dollar signs
+// 2. variable names must begin with a letter, $ or _
+// 3. Names are case-sensitive (y and Y are different variables)
+// 4. variable name should not be a reserved keyword (like var, let, const, function, etc.)
+// 5. variable names should be descriptive (like firstName, lastName, etc.
 
+// variables are of three types -- var, let, const
 var name = 'John'; // string
 var age = 30; // number
 var isStudent = true; // boolean
