52dc843836128f4e0ce94b5d790eb55cc1043811
aryan-jr-07 <aryanjr2010@gmail.com>
2025-07-23 23:51:04 +0530

    variables

---
+++ module 1/start.js
@@ -0,0 +1,6 @@
+// variables -- variable are container for storing data (storing data values)
+// variables are of three types -- var, let, const
+
+var name = 'John'; // string
+var age = 30; // number
+var isStudent = true; // boolean
