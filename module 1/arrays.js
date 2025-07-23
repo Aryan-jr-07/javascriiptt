ec0a6d4d77c062ae1922b4006494d1c4adee97fc
aryan-jr-07 <aryanjr2010@gmail.com>
2025-07-24 00:12:06 +0530

    arry

---
+++ module 1/arrays.js
@@ -0,0 +1,8 @@
+// array -- array are use to store multiple values in a single variable
+// arrays are of two types -- array literal and array constructor
+// array literal
+var fruits = ['apple', 'banana', 'cherry']; // array literal
+console.log(fruits);
+// array constructor
+var fruits = new Array('apple', 'banana', 'cherry'); // array constructor
+console.log(fruits);
