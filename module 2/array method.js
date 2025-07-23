5eed20064fd320564d40f0eb770d2ecf056f0cb9
aryan-jr-07 <aryanjr2010@gmail.com>
2025-07-24 02:43:47 +0530

    array method

---
+++ module 2/array method.js
@@ -0,0 +1,36 @@
+/*
+
+// inbuilt js array methods
+var arr2 = [1, 2, 3, 4, 5];
+console.log(arr2)
+
+// pop method -- removes the last element from an array
+var c = arr2.pop();
+console.log(c) // 5
+console.log("popped array:", arr2); // [1, 2, 3, 4]
+
+// push method -- adds a new element to the end of an array
+var d = arr2.push(100);
+console.log("pushed array:", arr2); // [1, 2, 3, 4, 100] 
+console.log(d); // this will return the new length of the array, the output will be 5 cuz the updated array length is 5
+
+
+
+
+// shift method -- remove the first element from an array
+var arr3 = [1, 2, 3, 4, 5];
+var e = arr3.shift(); // this will remove the first element (1) from the array
+console.log(e); // 1
+console.log(arr3); // [2, 3, 4, 5]
+var k = arr3.push(); // this will add 100 to the end of the array
+console.log(k); // 4 (this will give the new length of the array)
+console.log(arr3); // [2, 3, 4, 5]
+
+
+// unshift method -- adds a new element to the beginning of an array
+var arr3 = [1, 2, 3, 4, 5];
+var f = arr3.unshift(69); // this will add 69 to the beginning of the array
+console.log(arr3); // [69, 1, 2, 3, 4, 5]
+console.log(f); //6 // this will give the length of thr array after adding new element
+
+*/
