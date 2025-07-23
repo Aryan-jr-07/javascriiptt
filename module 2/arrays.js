6c08c1170ad54a644490a61af5a18ea15196c8ad
aryan-jr-07 <aryanjr2010@gmail.com>
2025-07-24 00:20:32 +0530

    module 2

--- module 2/arrays.js
+++ module 2/arrays.js
@@ -3,3 +3,13 @@
 // array provides you an ordered  collection of data
 var arr =[12, "bmw", true, null, undefined]
 console.log(arr);
+
+// you can access the elements of an array using their index
+console.log(arr[3]); // null
+// index starts from 0
+var a = arr[1]; // accessing the second element
+console.log(a); // bmw
+
+// you can also change the value of an element in an array
+arr[3] = "siuuu";  // changing the value at index 3 null to "siuuu" 
+console.log(arr[3]); // new value
