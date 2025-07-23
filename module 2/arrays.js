4ffdf8330106afc54e2eac9508b15c3b54024d49
aryan-jr-07 <aryanjr2010@gmail.com>
2025-07-24 00:16:50 +0530

    module 2

---
+++ module 2/arrays.js
@@ -0,0 +1,5 @@
+// array -- array are use to store multiple values in a single variable
+
+// array provides you an ordered  collection of data
+var arr =[12, "bmw", true, null, undefined]
+console.log(arr);
