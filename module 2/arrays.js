ecac99958c13b630cd14d18034d40b37c119706b
aryan-jr-07 <aryanjr2010@gmail.com>
2025-07-24 02:40:53 +0530

    array

--- module 2/arrays.js
+++ module 2/arrays.js
@@ -39,6 +39,7 @@ console.log(d); // this will return the new length of the array, the output will
 */
 
 
+/*
 // shift method -- remove the first element from an array
 var arr3 = [1, 2, 3, 4, 5];
 var e = arr3.shift(); // this will remove the first element (1) from the array
@@ -47,3 +48,10 @@ console.log(arr3); // [2, 3, 4, 5]
 var k = arr3.push(); // this will add 100 to the end of the array
 console.log(k); // 4 (this will give the new length of the array)
 console.log(arr3); // [2, 3, 4, 5]
+*/
+
+// unshift method -- adds a new element to the beginning of an array
+var arr3 = [1, 2, 3, 4, 5];
+var f = arr3.unshift(69); // this will add 69 to the beginning of the array
+console.log(arr3); // [69, 1, 2, 3, 4, 5]
+console.log(f); //6 // this will give the length of thr array after adding new element
