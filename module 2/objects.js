27817ebafbbba66351d138583ecc85c1c24d6e41
aryan-jr-07 <aryanjr2010@gmail.com>
2025-07-24 03:04:06 +0530

    updated

---
+++ module 2/objects.js
@@ -0,0 +1,20 @@
+// objects
+// since array are always ordered but the objects are unordered collection of data  
+
+var person={
+    name:"aryan",
+    age:21,
+    hobbies:["football","swimming","coding"],
+    address:{
+        city:"delhi",
+        road_no:12,
+        pincode:24010
+    }
+    
+}
+// console.log(person.address)
+// console.log(person.name) 
+// console.log(person.age)
+// console.log(person.address.city)
+
+// console.log(`Name: ${person.name}, Age: ${person.age}, Pincode: ${person.address.pincode}`)
