747159c205cf3b61b5497f21b674c7ea79390696
aryan-jr-07 <aryanjr2010@gmail.com>
2025-07-24 20:47:21 +0530

    for loop

---
+++ module 2/for_loops.js
@@ -0,0 +1,25 @@
+//  table of 9 
+// var a = 9;
+// for (var i = 1; i<11; i++){
+//     console.log(`${a} * ${i} = ${a*i}`); 
+// } 
+
+/* // you have an array and you have to square each element of that array
+var num = [1,2,3,4,5,6]
+var first = num[0] * num[0]
+var squaredArr = [first]
+var second = num[1] * num[1]
+squaredArr.push(second)
+var third = num[2] * num[2]
+squaredArr.push(third)
+console.log(squaredArr)    */
+
+
+// var num = [1,2,3,4,5,6]
+// var squaredArr =[]
+
+// for (var i=0; i<num.length; i++){
+//     squaredArr.push(num[i] * num[i])
+// }
+// console.log(squaredArr)
+
