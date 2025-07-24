f49b39fee1c243e842e30937da71b0716f346dae
aryan-jr-07 <aryanjr2010@gmail.com>
2025-07-24 17:18:38 +0530

    objects

--- module 2/objects.js
+++ module 2/objects.js
@@ -4,6 +4,7 @@
 var person={
     name:"aryan",
     age:21,
+    student:true,
     hobbies:["football","swimming","coding"],
     address:{
         city:"delhi",
@@ -12,9 +13,24 @@ var person={
     }
     
 }
+// console.log(person.hobbies[2])
 // console.log(person.address)
 // console.log(person.name) 
 // console.log(person.age)
 // console.log(person.address.city)
-
+/* another ways to do the same*/
 // console.log(`Name: ${person.name}, Age: ${person.age}, Pincode: ${person.address.pincode}`)
+
+// you can also change the value of an object property
+person.name="ronaldo";
+person.address.city="patna";
+person.student=false;
+// console.log(person.name) // ronaldo
+// console.log(person) // patna
+
+// you can also delete a property from an object
+delete person.address.road_no
+delete person.hobbies[0]
+console.log(person) 
+
+console.log(person.hobbies) 
