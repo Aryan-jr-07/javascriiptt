f52b18de6ae4e4277bf0fcf5c61e81bfb59b3f80
aryan-jr-07 <aryanjr2010@gmail.com>
2025-07-24 19:26:36 +0530

    elseif

---
+++ module 2/elseif.js
@@ -0,0 +1,21 @@
+// 90 to 100 - A 
+// 70 to 89 - B
+// 50 to 69 - C
+//  Lower than 50 - F
+/*
+var marks = 89;
+// marks = Number(marks)
+if (marks >=90 && marks <=100){
+    console.log("A")
+}
+else if(marks >=70 && marks <=89){
+    console.log("B")
+}
+else if(marks >=50 && marks <= 69){
+    console.log("C")
+}
+else {
+    console.log("F")
+}
+*/
+
