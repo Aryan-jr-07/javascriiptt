56aa4a2f14bfd21c2c132dd4a0f6f819eb3f11d8
aryan-jr-07 <aryanjr2010@gmail.com>
2025-07-28 05:22:59 +0530

    functions

---
+++ module 4/function.js
@@ -0,0 +1,39 @@
+// function demo() {
+//     console.log("This is a demo function from module 4.");
+// }
+// demo()
+
+// function add(a,c){
+//     console.log(`the sum of ${a} and ${c} is ${a+c}`)
+// }
+// add(10,2)
+// add(9,1)
+
+
+
+// function expression
+// var add = function(a,c){
+// return statement
+// var work = function(a,k){
+//     return a*k
+// }
+// let result = work(3,2)
+// console.log(result)
+
+
+
+
+
+// QUESTIONS :-
+// function add(){
+//     console.log(answer)
+//     var answer =2
+// }
+// add()
+
+
+// function fn(){
+//     return 4+5;
+// }
+// fn(3,7);
+// console.log(fn());
