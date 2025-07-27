e377531d749f32b0c9acb9162a9561bfd8aef57e
aryan-jr-07 <aryanjr2010@gmail.com>
2025-07-28 03:20:28 +0530

    do while

--- module 2/while_&_do_while.js
+++ module 2/while_&_do_while.js
@@ -1,10 +1,16 @@
-//  while loop example
-var i = 1
-var n =10
-while(i<=10) {
-    console.log(i);
-    i++;
-}
+// //  while loop example
+// var i = 1
+// var n =10
+// while(i<=10) {
+//     console.log(i);
+//     i++;
+// }
 
 
 //  do while loop example
+var i = 1
+var d = 10
+do {
+    console.log(i);
+    i++
+} while(i<=10);
