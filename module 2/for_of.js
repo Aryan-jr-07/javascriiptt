30bc7f8716c7cfe3e1f42369c191cd0c1707ddf6
aryan-jr-07 <aryanjr2010@gmail.com>
2025-07-28 04:18:49 +0530

    for of

--- module 2/for_of.js
+++ module 2/for_of.js
@@ -6,8 +6,13 @@
 
 // var score = [1, 44, 23, 97, 12];
 
-var score = ["John", "Doe", "Jane", "Smith"];
-for (var [index, value] of score.entries()){
-    console.log(`Index: ${index}, Value: ${value}`);
-}
+// var score = ["John", "Doe", "Jane", "Smith"];
+// for (var [index, value] of score.entries()){
+//     console.log(`Index: ${index}, Value: ${value}`);
+// }
 
+
+var c = "Newton";
+for(var i of c){
+    console.log(i)
+}
