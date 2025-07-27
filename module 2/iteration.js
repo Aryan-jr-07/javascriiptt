2bda865283c8ca6cb1b9c78e031273d33ccb34f2
aryan-jr-07 <aryanjr2010@gmail.com>
2025-07-28 03:43:50 +0530

    ittration

---
+++ module 2/iteration.js
@@ -0,0 +1,11 @@
+/* How to iterate an array*/
+// var players = ['ronaldo','messi','raul','vini']
+// for(var goal in players){
+//     console.log(players[goal]);
+// }
+
+
+// var ok =['ap','jp','mp','up']
+// for (var siuu in ok){
+//     console.log(ok[siuu])
+// }
--- module 2/while_&_do_while.js
+++ module 2/while_&_do_while.js
@@ -8,9 +8,24 @@
 
 
 //  do while loop example
-var i = 1
-var d = 10
-do {
-    console.log(i);
-    i++
-} while(i<=10);
+// var i = 1
+// var d = 10
+// do {
+//     console.log(i);
+//     i++
+// } while(i<=10);
+
+
+
+// var i ={
+//     first:"ronaldo",
+//     second:"messi",
+//     third:"raul"
+// }
+
+// for (var siu in i) {
+//     console.log(siu +' -> '+i[siu]);
+// }
+
+
+
